9de4da35416ffcca1863574fb0466676d66c7c50
viachaslaufamin <119659118+viachaslaufamin@users.noreply.github.com>
2023-08-22 12:37:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+//Задание1
+//console.log('старт');
+//for(let i=1; i<=9; i++){
+//   console.log(i);
+//}
+//console.log('финиш')
+
+
+//Задание 2
+
+//for(let i=10; i<100; i++){
+//   if(i % 3 ==0 && i % 5 == 0){
+//      console.log(i);
+//   }
+//}
+
+
+//Задание 3
+
+//let number = [20, 14, 4, 60, 7, 21, 13];
+
+//for(i=0; i<number.length; i++){
+//   if(number[i] >= 15){
+//      console.log(number[i]);
+//   }
+//}
+
+//Задание4
+
+//let number = [20, 14, 4, 60, 7, 21, 13];
+
+//for(let i=0; i<number.length; i++){
+//   if(number[i] % 2 !== 0){
+//      console.log(number[i]);
+//   }
+//}
+
+//Задание 5
+
+//let number = [20, 14, 4, 60, 7, 21, 13];
+
+//for(let i=0; i<number.length; i++){
+//   if(number[i] % 3 === 0){
+//      console.log(number[i]);;
+//   }
+//}
